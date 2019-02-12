5b5fb72dd806aeb41949a8e4a2cc703229ebd5d9
marianoar <47338236+marianoar@users.noreply.github.com>
2019-02-12 17:23:34 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,10 @@ function Mostrar()
 {
 //tomo la edad  
 
-
+var edad=document.getElementById("edad").value;
+if (edad>17) {
+    alert("usted tiene "+edad+" años. Es mayor de edad");
+    
+}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,10 @@
 function Mostrar()
 {
 //tomo la edad  
-
-
+var edad=document.getElementById("edad").value;
+if (edad>17) {
+    alert(" es mayor de edad");
+    } else {
+alert("es menor de edad");
+}
 }//FIN DE LA FUNCIÓN
